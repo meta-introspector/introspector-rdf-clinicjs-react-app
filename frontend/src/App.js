18d54041eb86bea9ac7577f211093983ce6bfe64
mike dupont <mike.dupont@introspector.local>
2024-10-01 14:29:03 -0400

    trying to package up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell
+} from "@nextui-org/table";
 
 function App() {
   return (
@@ -7,7 +15,41 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Experiments
+	  <Table
+              aria-label="Experiments"
+	  >
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+	  <TableColumn>Link</TableColumn>
+        </TableHeader>
+          <TableBody>
+	  <TableRow key="hf">
+	  <TableCell><pre><code>Hugging Face</code></pre></TableCell>
+	  <TableCell><a href="https://huggingface.co/introspector">Hugging Face</a></TableCell>
+	   </TableRow>
+
+	  <TableRow key="gh">
+	  <TableCell>
+	  <pre><code>Github Repositories</code></pre></TableCell>
+	  <TableCell><a href="https://github.com/meta-introspector">Github</a></TableCell>
+	  </TableRow>
+
+      	  <TableRow key="bp">
+	  <TableCell>
+	  <pre><code>Blog posts</code></pre></TableCell>
+	  <TableCell><a href="https://h4ck3rm1k3.wordpress.com">Wordpress</a></TableCell>	  
+	  </TableRow>
+
+      	  <TableRow key="tweets">
+	  <TableCell>
+	  <pre><code>Tweets</code></pre></TableCell>
+	  <TableCell><a href="https://x.com/introsp3ctor">Twitter</a></TableCell>	  
+	  </TableRow>
+  
+
+	  </TableBody>
+      </Table>
         </p>
         <a
           className="App-link"
---
+++ report.py
@@ -0,0 +1,14 @@
+# simple report
+# 1. list the datasets in my hugging face org
+# filter by some criteria
+# add to git submodules
+# 2. list the files in my git submodules
+# filter by some citeria
+# for each file calculate an absolute path to find the data
+# construct a rdf linked data file for each dataset, call it introspector.rdf.js put into each repository.
+# we can use json-ld
+# 3. populate a directory structure in json file
+# load json into js,
+# use a d3 js spring graph and interactive query system
+# display json on screen
+# and when item is selected select the iframe to display the url referenced.
