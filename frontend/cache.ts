26b806182fb4491fc31e435246055ffdf0ad604c
mike dupont <mike.dupont@introspector.local>
2024-10-09 15:19:49 -0400

    now reading the file

--- frontend/cache.ts
+++ frontend/cache.ts
@@ -5,12 +5,17 @@ export function associate_cache(url: string,
 				fn_parts:string[]) {
     const alen = url_parts.length-1;
     //const filename  = url_parts[url_parts.length-1];
-    console.log("cache url: " + url);
-    console.log("cache: parts:" + url_parts.slice(6,alen +1));
+    //console.log("cache url: " + url);
+    //console.log("cache: parts:" + url_parts.slice(6,alen +1));
     const hostname_base = hostname.split(".")[0]
-    const newpath = "../inputs/" + hostname_base + url_parts.slice(0,4).join("/") + "/"+ fn_parts.join(".")
-    console.log("cache: slice:" + newpath);
+    const newpath = "../inputs/" + hostname_base + url_parts.slice(0,4).join("/") + "/"+ fn_parts.join(".");
+    //console.log("cache: slice:" + newpath);
+    //const htmlStream = fs.createReadStream(newpath);
+    return {
+	"newpath" : newpath
+    };    
 }
+
 export function save_cache(url:string,
 			   hostname: string,
 			   url_parts:string[],
--- frontend/clinic_report.ts
+++ frontend/clinic_report.ts
@@ -2,8 +2,43 @@ import { Functions } from "./functions"
 import { missing } from "./missing"
 import { associate_cache, save_cache } from "./cache"
 
-function isp_clinic_flame_report(report_url:string) {   
-    return "flame report:" + report_url;    
+const fs = require('node:fs');
+
+function process_chunk(line:string)
+{
+    const begins = "module.exports="
+    if (line.includes("childrenVisibilityToggle") && (line.includes(begins))) {
+
+
+	const bl = begins.length;
+	//console.log("chunk: " + line.substring(bl,20));
+	const chunk = line.substring(bl,line.length);
+	let obj = JSON.parse(chunk)
+	//let runnable = eval(line);
+	//runnable.Run("module.exports").then((result:string)=>{console.log(result);});
+	console.log(obj);
+    }
+
+
+    "}"
+}
+
+function process_flame_report(err:any, data:string) {
+    //console.log("data: %o", data);
+    if (data) {	
+	data.split("\n").forEach(process_chunk);
+    }
+   
+
+
+}
+
+function isp_clinic_flame_report(report_url:string, data:any) {   
+    console.log("file: " + data.newpath);
+    fs.readFile(data.newpath, "utf-8",process_flame_report);
+
+    return "flame report:" + report_url;
+    
 }
 
 const clinic_functions: Functions = {
